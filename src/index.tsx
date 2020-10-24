ebe32ade2758d00d4dae536e1e58b6c69b4ba7f2
Andrés <andressanchezro@hotmail.com>
2020-10-24 22:19:00 +0200

    fea: add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import { routes } from './common';
+import ApiKeyPage from './pages/apiKey/apiKeyPage';
+import CharactersPage from './pages/characters/charactersPage';
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Router>
+      <Switch>
+        <Route exact={true} path={routes.apiKey} component={ApiKeyPage} />
+        <Route exact={true} path={routes.characters} component={CharactersPage} />
+        <Route path={"*"}>
+          <Redirect to={routes.apiKey} />
+        </Route>
+      </Switch>
+  </Router>,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
