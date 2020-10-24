8c7edb1817e95dea7638b28ccaa871f3a36034a9
Andrés <andressanchezro@hotmail.com>
2020-10-24 20:36:55 +0200

    fea: add common with utils

---
+++ src/common/utils.ts
@@ -0,0 +1,10 @@
+const keyGenerator = (): string => {
+    return (
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+    )
+}
+
+export const utils = {
+    keyGenerator
+}
