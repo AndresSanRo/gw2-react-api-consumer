cb550ee0312cd26fb7b94aa481bd3a786489502b
Andrés <andressanchezro@hotmail.com>
2020-10-25 19:56:02 +0100

    fea: add styles to input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,18 +1,28 @@
 import React, { FunctionComponent } from 'react';
+import './input.scss';
 
 interface InputProps {
-	type: string;
-	placeHolder: string;
+	type?: string;
+	placeHolder?: string;
 	label?: string;
+	required?: boolean;
 }
 
 export const Input: FunctionComponent<InputProps> = (
 	props: InputProps,
 ): JSX.Element => {
 	return (
-		<div>
-			{props.label !== null && <label>{props.label}</label>}
-			<input type={props.type} placeholder={props.placeHolder} />
+		<div className="inputContainer">
+			{props.label !== null && (
+				<label className={props.required ? 'required' : ''}>
+					{props.label}
+				</label>
+			)}
+			<input
+				required={props.required}
+				type={props.type ? props.type : 'text'}
+				placeholder={props.placeHolder ? props.placeHolder : ''}
+			/>
 		</div>
 	);
 };
