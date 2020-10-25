22f6f8277af10621c4a671b16cb41b13c94d35a9
Davos95 <davidvalencia_@hotmail.com>
2020-10-25 13:29:44 +0100

    features: add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import { routes } from './common';
-import ApiKeyPage from './pages/apiKey/apiKeyPage';
-import CharactersPage from './pages/characters/charactersPage';
+import { routes } from "./common";
+import ApiKeyPage from "./pages/apiKey/apiKeyPage";
+import CharactersPage from "./pages/characters/charactersPage";
 
 ReactDOM.render(
   <Router>
-      <Switch>
-        <Route exact={true} path={routes.apiKey} component={ApiKeyPage} />
-        <Route exact={true} path={routes.characters} component={CharactersPage} />
-        <Route path={"*"}>
-          <Redirect to={routes.apiKey} />
-        </Route>
-      </Switch>
+    <Switch>
+      <Route exact={true} path={routes.apiKey} component={ApiKeyPage} />
+      <Route exact={true} path={routes.characters} component={CharactersPage} />
+      <Route path={"*"}>
+        <Redirect to={routes.apiKey} />
+      </Route>
+    </Switch>
   </Router>,
-document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/apiKey/apiKeyPage.tsx
+++ src/pages/apiKey/apiKeyPage.tsx
@@ -1,11 +1,10 @@
 import React, { FunctionComponent } from "react";
 import { Input } from "../../components";
-
+import { Button } from "react-bootstrap";
 const ApiKeyPage: FunctionComponent = (): JSX.Element => {
   return (
     <>
       <h1>API key page</h1>
-      <Input placeHolder={"HOLAAAA"} type={"text"} label={"HEY"} />
     </>
   );
 };
