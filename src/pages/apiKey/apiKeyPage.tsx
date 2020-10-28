454b312cf8dffe8977228539ac56b6f93164a36e
Davos95 <davidvalencia_@hotmail.com>
2020-10-28 19:19:58 +0100

    features: custom hook form apiKey

--- src/pages/apiKey/apiKeyPage.tsx
+++ src/pages/apiKey/apiKeyPage.tsx
@@ -1,13 +1,21 @@
-import React, { FunctionComponent } from 'react';
-import { Input, Button } from '../../components';
+import React, { FunctionComponent } from "react";
+import { Input, Button } from "../../components";
+import { useApiKey } from "./useApiKey";
 const ApiKeyPage: FunctionComponent = (): JSX.Element => {
-	return (
-		<>
-			<h1>API key page</h1>
-			<Input label={'API key'} required={true} />
-			<Button text={'hey'} onClick={() => {}} />
-		</>
-	);
+  const { apiKey, setApiKey, saveApiKey } = useApiKey();
+
+  return (
+    <>
+      <h1>API key page</h1>
+      <Input
+        label={"API key"}
+        required={true}
+        onChangeValue={setApiKey}
+        value={apiKey}
+      />
+      <Button text={"Enviar"} onClick={saveApiKey} />
+    </>
+  );
 };
 
 export default ApiKeyPage;
