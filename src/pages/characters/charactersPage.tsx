b53f8d04005fab846d4bee2e091ad2371cd42f56
Andrés <andressanchezro@hotmail.com>
2020-10-31 13:46:14 +0100

    fea: add api call to fetch characters in characters page

--- src/pages/characters/charactersPage.tsx
+++ src/pages/characters/charactersPage.tsx
@@ -1,7 +1,25 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { gw2Api } from '../../api';
+import { Character, HttpResponse } from '../../model';
 
 const CharactersPage: FunctionComponent = (): JSX.Element => {
-    return <h1>Characters page</h1>
-}
+	const [characters, setCharacters] = useState<Character[]>([]);
+
+	useEffect(() => {
+		const fetchCharacters = async () => {
+			await gw2Api.characters
+				.getCharacters()
+				.then((response: HttpResponse<Character[]>) => {
+					if (response.parsedBody) {
+						setCharacters(response.parsedBody);
+						console.log(characters);
+					}
+				});
+		};
+		fetchCharacters();
+	}, [characters, setCharacters]);
+
+	return <h1>Characters page</h1>;
+};
 
 export default CharactersPage;
